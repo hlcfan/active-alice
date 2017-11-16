2b7f37914f25bbb71058e47a365e85a31fabb955
Adam Zaninovich <adam.zaninovich@gmail.com>
2017-11-16 11:06:38 -0800

    update alice to latest version and fix deps

--- mix.exs
+++ mix.exs
@@ -3,37 +3,39 @@ defmodule ActiveAlice.Mixfile do
 
   def project do
     [ app: :active_alice,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps ]
+      deps: deps() ]
   end
 
   def application do
-    [ applications: [:alice],
-      mod: {
-        Alice, [
-          Alice.Handlers.Random,
-          Alice.Handlers.OhYouSo,
-          Alice.Handlers.GoogleImages,
-          Alice.Handlers.Karma,
-          Alice.Handlers.Shizzle,
-          Alice.Handlers.Xkcd
-          Alice.Handlers.Wiki
-        ] } ]
+    [
+      applications: [:alice],
+      mod: {Alice, %{handlers: handlers()}}
+    ]
+  end
+
+  defp handlers do
+    [
+      Alice.Handlers.Random,
+      Alice.Handlers.OhYouSo,
+      Alice.Handlers.GoogleImages,
+      Alice.Handlers.Karma,
+      Alice.Handlers.Shizzle,
+      Alice.Handlers.Wiki
+    ]
   end
 
   defp deps do
      [
-       {:websocket_client, github: "jeremyong/websocket_client"},
-       # {:alice, path: "~/projects/alice/alice"}
-       {:alice,                  "~> 0.2.3"},
-       {:alice_google_images,    "~> 0.1.1"},
-       {:alice_karma,            "~> 0.1.0"},
-       {:alice_shizzle,          "~> 0.1.1"},
-       {:alice_xkcd,             "~> 0.0.1"},
-       {:alice_wiki,             "~> 1.0.0"}
+       # {:alice, github: "alice-bot/alice", branch: "master", override: true},
+       {:alice,               "~> 0.3"},
+       {:alice_google_images, "~> 0.1"},
+       {:alice_karma,         "~> 0.1"},
+       {:alice_shizzle,       "~> 0.1"},
+       {:alice_wiki,          "~> 1.0"}
      ]
   end
 end
